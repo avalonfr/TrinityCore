51e1694b286ca64995cc769d1b33067bb221aced
Eze <Administrateur@.(none)>
2012-01-25 10:52:11 +0100

    Modif argent tournament Thx Souler

--- src/server/scripts/Northrend/ArgentTournament.cpp
+++ src/server/scripts/Northrend/ArgentTournament.cpp
@@ -2792,108 +2792,9 @@ Spell Fixed:
 
 DELETE FROM `spell_script_names` WHERE `spell_id` IN (62960,62575,62544,62863);
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
-('62960', 'spell_tournament_charge'),
-('62575', 'spell_tournament_shield'),
 ('62544', 'spell_tournament_melee'),
 ('62863', 'spell_tournament_duel');
 ######*/
-
-class spell_tournament_charge : public SpellScriptLoader
-{
-public:
-    spell_tournament_charge() : SpellScriptLoader("spell_tournament_charge") { }
-
-    class spell_tournament_charge_SpellScript : public SpellScript
-    {
-        PrepareSpellScript(spell_tournament_charge_SpellScript);
-
-        void HandleEffectScriptEffect(SpellEffIndex /*effIndex*/)
-        {
-            if (Unit* pTarget = GetHitUnit())
-            {
-                if (Unit *caster = GetCaster())
-                {
-                    caster->CastSpell(pTarget,74399,true);
-                    caster->CastSpell(pTarget,68321,true);
-
-                    if (pTarget->GetTypeId() == TYPEID_UNIT && pTarget->ToCreature()->GetEntry() == 33272)
-                    {
-                        // Kill Credit
-                        if (Unit *player = caster->GetCharmerOrOwner())
-                            player->CastSpell(player,62658,true);
-                    }
-                }
-
-                if (pTarget->GetAura(64100))
-                    pTarget->RemoveAuraFromStack(64100);
-                else 
-                    if (pTarget->GetAura(62552))
-                        pTarget->RemoveAuraFromStack(62552);
-                    else 
-                        if (pTarget->GetAura(62719))
-                            pTarget->RemoveAuraFromStack(62719);
-            }
-        }
-
-        void Register()
-        {
-            OnEffectHitTarget += SpellEffectFn(spell_tournament_charge_SpellScript::HandleEffectScriptEffect, EFFECT_0, SPELL_EFFECT_SCRIPT_EFFECT);
-        }
-    };
-
-    SpellScript *GetSpellScript() const
-    {
-        return new spell_tournament_charge_SpellScript();
-    }
-};
-
-class spell_tournament_shield : public SpellScriptLoader
-{
-public:
-    spell_tournament_shield() : SpellScriptLoader("spell_tournament_shield") { }
-
-    class spell_tournament_shield_SpellScript : public SpellScript
-    {
-        PrepareSpellScript(spell_tournament_shield_SpellScript);
-
-        void HandleEffectScriptEffect(SpellEffIndex /*effIndex*/)
-        {
-            if (Unit* pTarget = GetHitUnit())
-            {
-                if (Unit *caster = GetCaster())
-                {
-                    caster->CastSpell(pTarget,62626,true );
-
-                    if (pTarget->GetTypeId() == TYPEID_UNIT && pTarget->ToCreature()->GetEntry() == 33243)
-                    {
-                        // Kill Credit
-                        if (Unit *player = caster->GetCharmerOrOwner())
-                            player->CastSpell(player,62673,true);
-                    }
-                }
-
-                if (pTarget->GetAura(64100))
-                    pTarget->RemoveAuraFromStack(64100);
-                else 
-                    if (pTarget->GetAura(62552))
-                        pTarget->RemoveAuraFromStack(62552);
-                    else 
-                        if (pTarget->GetAura(62719))
-                            pTarget->RemoveAuraFromStack(62719);
-            }
-        }
-
-        void Register()
-        {
-            OnEffectHitTarget += SpellEffectFn(spell_tournament_shield_SpellScript::HandleEffectScriptEffect, EFFECT_0, SPELL_EFFECT_SCRIPT_EFFECT);
-        }
-    };
-
-    SpellScript *GetSpellScript() const
-    {
-        return new spell_tournament_shield_SpellScript();
-    }
-};
 
 class spell_tournament_melee : public SpellScriptLoader
 {
@@ -3096,8 +2997,6 @@ public:
 void AddSC_Argent_Tournament()
 {
     new npc_chillmaw;
-    new spell_tournament_charge;
-    new spell_tournament_shield;
     new spell_tournament_melee;
     new spell_tournament_duel;
     new npc_dame_evniki_kapsalis;
