4293162c77392b973f3894247f4c8bd4da00297b
avalonfr <avalonfr_fr@yahoo.fr>
2012-02-04 20:49:49 +0100

    debut debug azol nerub Hadronox

--- src/server/scripts/Northrend/AzjolNerub/AzjolNerub/boss_hadronox.cpp
+++ src/server/scripts/Northrend/AzjolNerub/AzjolNerub/boss_hadronox.cpp
@@ -35,6 +35,7 @@ enum Spells
     H_SPELL_WEB_GRAB          = 59421,
     SPELL_WEB_FRONT_DOORS     = 53177,
     SPELL_WEB_SIDE_DOORS      = 53185,
+
     // anubar crusher
     SPELL_FRENZY              = 53801,
     SPELL_SMASH               = 53318,
@@ -108,7 +109,11 @@ class boss_hadronox : public CreatureScript
                 me->SetReactState(REACT_PASSIVE);
                 me->SetFloatValue(UNIT_FIELD_BOUNDINGRADIUS, 9.0f);
                 me->SetFloatValue(UNIT_FIELD_COMBATREACH, 9.0f);
+				me->GetMotionMaster()->Clear();
+				me->GetMotionMaster()->MovePoint(50,me->GetHomePosition());
+				sLog->outError("RESET *******************************************");
 
+				_WaveCount = 0;
                 _wpCount = 0;
                 _wpReached = false;
                 _movementStarted = false;
@@ -124,14 +129,14 @@ class boss_hadronox : public CreatureScript
 
             void JustSummoned(Creature* summon)
             {
-                _summons.Summon(summon);
+                _summons.push_back(summon->GetGUID()); //.Summon(summon);
             }
 
             void MovementInform(uint32 type, uint32 id)
             {
                 if (type != POINT_MOTION_TYPE || id != _wpCount)
                     return;
-
+				sLog->outError("point id  %u *************************",id);
                 if (id == 10)
                 {
                     // TODO: web doors
@@ -200,9 +205,9 @@ class boss_hadronox : public CreatureScript
                     return;
                 }
 
-                _EnterEvadeMode();
-                me->SetHomePosition(_home);
-                me->GetMotionMaster()->MoveTargetedHome();
+                //_EnterEvadeMode();
+                //me->SetHomePosition(_home);
+                //me->GetMotionMaster()->MoveTargetedHome();
                 Reset();
             }
 
@@ -215,7 +220,7 @@ class boss_hadronox : public CreatureScript
                     _events.ScheduleEvent(EVENT_CLOUD, urand(5, 10) *IN_MILLISECONDS);
                     _events.ScheduleEvent(EVENT_LEECH, urand(7, 12) *IN_MILLISECONDS);
                     _events.ScheduleEvent(EVENT_UNSTUCK, 3*IN_MILLISECONDS);
-                    _events.ScheduleEvent(EVENT_SPAWN, 1*IN_MILLISECONDS);
+                    _events.ScheduleEvent(EVENT_SPAWN, 1.5*IN_MILLISECONDS);
                 }
 
                 if (_wpReached)
@@ -264,7 +269,9 @@ class boss_hadronox : public CreatureScript
                             break;
                         case EVENT_SPAWN:
                             me->SummonCreature(RAND(NPC_ADD_CHAMPION, NPC_ADD_CRYPT_FIEND, NPC_ADD_NECROMANCER), AddWaypoints[0]);
-                            _events.ScheduleEvent(EVENT_SPAWN, urand(1500, 3000));
+							_WaveCount++;
+							if (_WaveCount <= 50 )  //prevent too much spawn & freeze serv
+								_events.ScheduleEvent(EVENT_SPAWN, urand(1500, 3000));
                             break;
                         case EVENT_FORCEMOVE:
                             me->SetReactState(REACT_PASSIVE);
@@ -294,6 +301,7 @@ class boss_hadronox : public CreatureScript
             EventMap _events;
             Position _home;
             uint8 _wpCount;
+			uint8 _WaveCount;
             bool _wpReached;
             bool _movementStarted;
             bool _engaged;
@@ -324,6 +332,9 @@ class npc_anubar_crusher : public CreatureScript
 
                 if (_instance)
                     _instance->SetData(DATA_HADRONOX_EVENT, NOT_STARTED);
+
+				if (Creature* Hadronox = ObjectAccessor::GetCreature(*me, _instance->GetData64(DATA_HADRONOX)))
+					Hadronox->AI()->Reset();
 
                 if (Creature* champion = GetClosestCreatureWithEntry(me, NPC_ANUBAR_CHAMPION, 200.0f, false))
                     champion->Respawn();
