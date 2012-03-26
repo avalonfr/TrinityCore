a9795b36acd87b40b0d60718dc18932b2436496b
Eze <4CSSI@AFSI19641.domaine.local>
2012-03-26 20:18:21 +0200

    Fix détournement

--- src/server/game/Spells/Auras/SpellAuraEffects.cpp
+++ src/server/game/Spells/Auras/SpellAuraEffects.cpp
@@ -4990,11 +4990,12 @@ void AuraEffect::HandleAuraDummy(AuraApplication const* aurApp, uint8 mode, bool
                     switch (GetId())
                     {
                         case 34477: // Misdirection
-                            if (aurApp->GetRemoveMode() != AURA_REMOVE_BY_EXPIRE)
+                            if (!GetBase()->GetDuration())
                                 target->SetReducedThreatPercent(0, 0);
                             break;
                         case 35079: // Misdirection proc
-                            target->SetReducedThreatPercent(0, 0);
+                            if (caster)
+                                caster->SetReducedThreatPercent(0, 0);
                             break;
                     }
                     break;
