f19624e5e0b296903ece62c2644f1dc1b4fe2a04
Eze <4CSSI@AFSI19641.domaine.local>
2012-03-28 16:03:36 +0200

    Fix spell Sanguinaire

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -2994,6 +2994,17 @@ void SpellMgr::LoadDbcDataCorrections()
             case 24259: // Spell Lock
                 spellInfo->speed = 80;
                 break;
+                // make it capable of crit as magic effect using spell crit chance
+                spellInfo->AttributesEx2 &= ~SPELL_ATTR2_CANT_CRIT;
+                spellInfo->DmgClass = SPELL_DAMAGE_CLASS_MAGIC;
+                spellInfo->SchoolMask = SPELL_SCHOOL_MASK_HOLY;
+                break;
+            case 23881: // Bloodthirst
+                spellInfo->EffectImplicitTargetA[EFFECT_1] = TARGET_UNIT_CASTER;
+                break;
+            case 23885: // Bloodthirst
+                spellInfo->EffectApplyAuraName[EFFECT_0] = SPELL_AURA_PROC_TRIGGER_SPELL_WITH_VALUE;
+                break;
 			case 48278:
                 spellInfo->Targets = 1;
                 spellInfo->StackAmount = 3;
