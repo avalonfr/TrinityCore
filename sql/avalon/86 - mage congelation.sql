fef21297823a0c1f16324a0a5583f0f72f373de9
s@m <avalonfr_fr@yahoo.fr>
2012-03-28 17:27:41 +0200

    mago congelation

---
+++ sql/avalon/86 - mage congelation.sql
@@ -0,0 +1,3 @@
+delete FROM spell_proc_event WHERE entry IN (71761);
+INSERT INTO `spell_proc_event` (`SchoolMask`, `ppmRate`, `CustomChance`, `entry`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `Cooldown`)
+VALUES (0, 0, 0, 71761, 3, 0, 1048576, 0, 0, 256, 0);
