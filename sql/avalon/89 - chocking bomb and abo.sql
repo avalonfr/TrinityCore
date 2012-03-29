53f21d5e2d5e83697b7c299b9d0da1a1058cee6c
s@m <avalonfr_fr@yahoo.fr>
2012-03-29 14:14:17 +0200

    exclude abo from possible target of chocking bomb s@m

---
+++ sql/avalon/89 - chocking bomb and abo.sql
@@ -0,0 +1,9 @@
+/*
+SQLyog Enterprise - MySQL GUI v8.18 
+MySQL - 5.1.53-community-log 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+delete FROM conditions WHERE SourceEntry = 71255;
+insert into `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorTextId`, `ScriptName`, `Comment`) values('13','1','71255','0','0','31','0','3','37672','0','1','0','',NULL);
+insert into `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorTextId`, `ScriptName`, `Comment`) values('13','2','71255','0','0','31','0','3','38285','0','1','0','',NULL);
