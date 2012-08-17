ad1d164411ab107e93a4e82cf400dfd467f406a7
unknown <Fduret@FRY9A5UT.(none)>
2012-08-17 17:33:54 +0200

    miss assign scriptname

--- sql/avalon/111- Icc_Buff.sql
+++ sql/avalon/111- Icc_Buff.sql
@@ -1,2 +1,3 @@
 /* disable  ICC Buff */
 UPDATE spell_area SET autocast = 0 WHERE AREA = 4812 AND (spell = 73822 OR spell = 73828);
+UPDATE creature_template  SET scriptname = "npc_choose_icc_buff" WHERE entry = 39372;
